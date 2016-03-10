9dcc1595d9ad3990a6ba9dc5fbcb4d6d910c77c2
kaushik gotefode <kaushikgotefode@gmail.com>
2016-03-10 20:00:28 +0530

    session 6

--- assignments/js/app/app.js
+++ assignments/js/app/app.js
@@ -1,60 +1,202 @@
 var app = angular.module("myApp", ['jkuri.datepicker']);
 app.controller("ExpenseController", ['$scope', function($scope) {
     $scope.name = "Kaushik";
-    $scope.addData = true;
-    $scope.expenses = [];
+    $scope.addDataTr = false;
+    $scope.trans = {};
     $scope.typeFilter = ""
-        
-    // $scope.transactions = window.localStorage.getItem('transactions',JSON.stringify($scope.transactions));
-    $scope.reset = function(){
-        $scope.payer = null;
-        $scope.payee = null ;       
-        $scope.amount = null;        
-        $scope.date = null ;       
-        $scope.paymentMode = null;        
-        $scope.notes = null;         
-        $scope.type = null;
+    $scope.type = "Expense"
+    $scope.toggleBtnText = "Add +"
+    $scope.addNew = "Add"
 
-    }
-    $scope.addNewData = function(){
-        postdata = {
-            id: 1,
-            payer: $scope.payer,
-            payee: $scope.payee,
-            amount: $scope.amount,
-            date: $scope.date,
-            modeOfPayment: $scope.paymentMode,
-            notes: $scope.notes,
-            type: $scope.type
+    $scope.reset = function() {
+            $scope.trans = {};
+        }
+        // $scope.addNewData = function() {
+        //     postdata = {
+        //         id: $scope.id,
+        //         payer: $scope.payer,
+        //         payee: $scope.payee,
+        //         amount: $scope.amount,
+        //         date: $scope.date,
+        //         modeOfPayment: $scope.paymentMode,
+        //         notes: $scope.notes,
+        //         type: $scope.type
 
-        };
-        $scope.localTransection.push(postdata)
-        window.localStorage.setItem('transaction', angular.toJson($scope.localTransection));
+    //     };
+    //     $scope.localTransection.push(postdata)
+    //     window.localStorage.setItem('transaction', angular.toJson($scope.localTransection));
+    //     $scope.reset();
+    // };
+
+    $scope.addIncome = function(postdata) {
+        // postdata = {
+        //     id: $scope.id,
+        //     payer: $scope.payer,
+        //     payee: $scope.payee,
+        //     amount: $scope.amount,
+        //     date: $scope.date,
+        //     modeOfPayment: $scope.paymentMode,
+        //     notes: $scope.notes,
+        //     type: "Income"
+
+        // };
+        $scope.localIncome.push(postdata);
+        window.localStorage.setItem('income', angular.toJson($scope.localIncome));
         $scope.reset();
+        $scope.addNew = "Add";
     };
+    $scope.addExpense = function(postdata) {
+        // postdata = {
+        //     id: $scope.id,
+        //     payer: $scope.payer,
+        //     payee: $scope.payee,
+        //     amount: $scope.amount,
+        //     date: $scope.date,
+        //     modeOfPayment: $scope.paymentMode,
+        //     notes: $scope.notes,
+        //     type: "Expense"
 
-    $scope.editData=function(i){
-
-
+        // };
+        $scope.localExpense.push(postdata);
+        window.localStorage.setItem('expense', angular.toJson($scope.localExpense));
+        $scope.reset();
+        $scope.addNew = "Add";
+    };
+    $scope.editData = function(i) {
+        $scope.trans = $scope.localTransection[i]
+        $scope.localTransection.splice(i, 1);
+        $scope.addNew = "Update";
+        $scope.addDataTr = true;
     }
-    $scope.showExpense= function(){
+    // $scope.editExpense = function(i) {
+    //     $scope.trans = $scope.localTransection[i]
+    //     $scope.localTransection.splice(i, 1);
+
+    // }
+
+    $scope.showExpense = function() {
+        // $scope.typeFilter = "Expense"
+        $scope.type = "Expense";
+        $scope.localTransection = $scope.localExpense;
+        $scope.addDataTr = false;
+    }
+
+    $scope.showIncome = function() {
+        // $scope.typeFilter = "Income"
+        $scope.type = "Income";
+        $scope.localTransection = $scope.localIncome;
+        $scope.addDataTr = false;
+    }
+    $scope.addData = function() {
+        if($scope.addDataTr){
+            $scope.addDataTr = false;
+            $scope.toggleBtnText = "Add +"
+        }else{
+            $scope.addDataTr = true;
+            $scope.toggleBtnText = "Remove -"
+
+        }
+
         
-        $scope.typeFilter= "Expense"
-
-    }
-     $scope.showIncome= function(){
-        
-        $scope.typeFilter= "Income"
-
-    }
-     $scope.showTransection= function(){
-        
-        $scope.typeFilter= ""
-
     }
 
-    
-    $scope.transactions = [{
+    // $scope.showTransection = function() {
+    //     $scope.typeFilter = ""
+    // }
+
+
+    // $scope.transactions = [{
+    //     "id": 1,
+    //     "payer": "Gretchen Hunter",
+    //     "payee": "Isabel Saunders",
+    //     "amount": "100.00",
+    //     "date": "December 17, 2015",
+    //     "modeOfPayment": "Cash",
+    //     "notes": "Dinner Cash",
+    //     "type": "Income"
+    // }, {
+    //     "id": 2,
+    //     "payer": "Isabel Saunders",
+    //     "payee": "Caroline Adkins",
+    //     "amount": "50.00",
+    //     "date": "December 28, 2015",
+    //     "modeOfPayment": "Electronic Transfer",
+    //     "notes": "Money Owed",
+    //     "type": "Expense"
+    // }, {
+    //     "id": 3,
+    //     "payer": "Isabel Saunders",
+    //     "payee": "Wall-E Mart Inc.",
+    //     "amount": "280.00",
+    //     "date": "December 31, 2015",
+    //     "modeOfPayment": "Credit Card",
+    //     "notes": "New Year's Gift",
+    //     "type": "Expense"
+    // }, {
+    //     "id": 4,
+    //     "payer": "Marian Floyd",
+    //     "payee": "Isabel Saunders",
+    //     "amount": "98.00",
+    //     "date": "January 04, 2016",
+    //     "modeOfPayment": "Cash",
+    //     "notes": "Dinner Share",
+    //     "type": "Income"
+    // }, {
+    //     "id": 5,
+    //     "payer": "Random Inc.",
+    //     "payee": "Isabel Saunders",
+    //     "amount": "2500.00",
+    //     "date": "January 04, 2016",
+    //     "modeOfPayment": "Electronic Transfer",
+    //     "notes": "Salary",
+    //     "type": "Income"
+    // }, {
+    //     "id": 6,
+    //     "payer": "Isabel Saunders",
+    //     "payee": "Gretchen Hunter",
+    //     "amount": "250.00",
+    //     "date": "January 05, 2016",
+    //     "modeOfPayment": "Electronic Transfer",
+    //     "notes": "Rent Share",
+    //     "type": "Expense"
+    // }, {
+    //     "id": 7,
+    //     "payer": "Isabel Saunders",
+    //     "payee": "Wall-E Mart Inc.",
+    //     "amount": "100.00",
+    //     "date": "January 07, 2016",
+    //     "modeOfPayment": "Credit Card",
+    //     "notes": "Monthly Provisions",
+    //     "type": "Expense"
+    // }, {
+    //     "id": 8,
+    //     "payer": "BitiBank LTD.",
+    //     "payee": "Isabel Saunders",
+    //     "amount": "500.00",
+    //     "date": "January 10, 2016",
+    //     "modeOfPayment": "Electronic Transfer",
+    //     "notes": "Credit Card Payment",
+    //     "type": "Expense"
+    // }, {
+    //     "id": 9,
+    //     "payer": "New Venture Inc.",
+    //     "payee": "Isabel Saunders",
+    //     "amount": "600.00",
+    //     "date": "January 15, 2016",
+    //     "modeOfPayment": "Cash",
+    //     "notes": "Side Project Payment",
+    //     "type": "Income"
+    // }, {
+    //     "id": 10,
+    //     "payer": "Isabel Saunders",
+    //     "payee": "Drunkards Pub",
+    //     "amount": "100.00",
+    //     "date": "January 15, 2016",
+    //     "modeOfPayment": "Credit Card",
+    //     "notes": "Night Out",
+    //     "type": "Expense"
+    // }]
+    $scope.incomes = [{
         "id": 1,
         "payer": "Gretchen Hunter",
         "payee": "Isabel Saunders",
@@ -65,24 +207,6 @@ app.controller("ExpenseController", ['$scope', function($scope) {
         "type": "Income"
     }, {
         "id": 2,
-        "payer": "Isabel Saunders",
-        "payee": "Caroline Adkins",
-        "amount": "50.00",
-        "date": "December 28, 2015",
-        "modeOfPayment": "Electronic Transfer",
-        "notes": "Money Owed",
-        "type": "Expense"
-    }, {
-        "id": 3,
-        "payer": "Isabel Saunders",
-        "payee": "Wall-E Mart Inc.",
-        "amount": "280.00",
-        "date": "December 31, 2015",
-        "modeOfPayment": "Credit Card",
-        "notes": "New Year's Gift",
-        "type": "Expense"
-    }, {
-        "id": 4,
         "payer": "Marian Floyd",
         "payee": "Isabel Saunders",
         "amount": "98.00",
@@ -91,7 +215,7 @@ app.controller("ExpenseController", ['$scope', function($scope) {
         "notes": "Dinner Share",
         "type": "Income"
     }, {
-        "id": 5,
+        "id": 3,
         "payer": "Random Inc.",
         "payee": "Isabel Saunders",
         "amount": "2500.00",
@@ -100,7 +224,36 @@ app.controller("ExpenseController", ['$scope', function($scope) {
         "notes": "Salary",
         "type": "Income"
     }, {
-        "id": 6,
+        "id": 4,
+        "payer": "New Venture Inc.",
+        "payee": "Isabel Saunders",
+        "amount": "600.00",
+        "date": "January 15, 2016",
+        "modeOfPayment": "Cash",
+        "notes": "Side Project Payment",
+        "type": "Income"
+    }]
+
+    $scope.expenses = [{
+        "id": 1,
+        "payer": "Isabel Saunders",
+        "payee": "Caroline Adkins",
+        "amount": "50.00",
+        "date": "December 28, 2015",
+        "modeOfPayment": "Electronic Transfer",
+        "notes": "Money Owed",
+        "type": "Expense"
+    }, {
+        "id": 2,
+        "payer": "Isabel Saunders",
+        "payee": "Wall-E Mart Inc.",
+        "amount": "280.00",
+        "date": "December 31, 2015",
+        "modeOfPayment": "Credit Card",
+        "notes": "New Year's Gift",
+        "type": "Expense"
+    }, {
+        "id": 3,
         "payer": "Isabel Saunders",
         "payee": "Gretchen Hunter",
         "amount": "250.00",
@@ -109,7 +262,7 @@ app.controller("ExpenseController", ['$scope', function($scope) {
         "notes": "Rent Share",
         "type": "Expense"
     }, {
-        "id": 7,
+        "id": 4,
         "payer": "Isabel Saunders",
         "payee": "Wall-E Mart Inc.",
         "amount": "100.00",
@@ -118,7 +271,7 @@ app.controller("ExpenseController", ['$scope', function($scope) {
         "notes": "Monthly Provisions",
         "type": "Expense"
     }, {
-        "id": 8,
+        "id": 5,
         "payer": "BitiBank LTD.",
         "payee": "Isabel Saunders",
         "amount": "500.00",
@@ -127,16 +280,7 @@ app.controller("ExpenseController", ['$scope', function($scope) {
         "notes": "Credit Card Payment",
         "type": "Expense"
     }, {
-        "id": 9,
-        "payer": "New Venture Inc.",
-        "payee": "Isabel Saunders",
-        "amount": "600.00",
-        "date": "January 15, 2016",
-        "modeOfPayment": "Cash",
-        "notes": "Side Project Payment",
-        "type": "Income"
-    }, {
-        "id": 10,
+        "id": 6,
         "payer": "Isabel Saunders",
         "payee": "Drunkards Pub",
         "amount": "100.00",
@@ -146,16 +290,20 @@ app.controller("ExpenseController", ['$scope', function($scope) {
         "type": "Expense"
     }]
 
-    if (!window.localStorage.getItem('transaction')){
-        $scope.transaction = window.localStorage.setItem('transaction', JSON.stringify($scope.transactions));
-    }
-    $scope.localTransection= JSON.parse(window.localStorage.getItem('transaction'))
-    // if(!window.localStorage.getItem('transactions')){
-    //     $scope.transactions = JSON.parse(window.localStorage.getItem($scope.transactions));
+    // if (!window.localStorage.getItem('transaction')) {
+    //     $scope.transaction = window.localStorage.setItem('transaction', JSON.stringify($scope.transactions));
     // }
-    // else {
-    //     window.localStorage.setItem('transactions', JSON.stringify($scope.transactions));
-    //     $scope.transactions = JSON.parse(window.localStorage.getItem('transactions'));
-    // }
+    // $scope.localTransection = JSON.parse(window.localStorage.getItem('transaction'));
 
+    if (!window.localStorage.getItem('income')) {
+        $scope.income = window.localStorage.setItem('income', JSON.stringify($scope.incomes));
+    }
+    $scope.localIncome = JSON.parse(window.localStorage.getItem('income'));
+
+    if (!window.localStorage.getItem('expense')) {
+        $scope.expense = window.localStorage.setItem('expense', JSON.stringify($scope.expenses));
+    }
+    $scope.localExpense = JSON.parse(window.localStorage.getItem('expense'));
+
+    $scope.localTransection = $scope.localExpense;
 }]);
